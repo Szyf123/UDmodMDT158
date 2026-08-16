84d4111183e02d56594a200a95d6b14c356c1e34
Szyf123 <3411327940@qq.com>
2026-08-16 10:05:32 +0800

    添加物品

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// scripts/main.js
+// 注册自定义属性，用于地板属性系统
+const shallowSea = Attribute.add("shallow-sea");
+const deepSea = Attribute.add("deep-sea");
+
+// 可选：打印日志以确认注册成功
+print("自定义属性已注册: shallow-sea, deep-sea");
